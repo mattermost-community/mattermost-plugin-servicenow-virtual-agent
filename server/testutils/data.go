ce254ef1074958929cef1deb1b4fea493dcb7cf3
Manoj Malik <manoj@brightscout.com>
2022-12-22 17:34:41 +0530

    [MI-2488] Fixed the low severity security issue regarding file ownership (#8)
    * [MI-2488] Fixed the low severity security issue regarding file ownership
    Added a check for if file is deleted or not before sending it to VA
    
    * [MI-2488] Added a check for creator ID as well when fetching file info before sending to VA
    Added some new tests and fixed the failing unit tests
    Created some new testutils
    
    * [MI-2488] Fix lint error
    
    * Update server/plugin/virtual_agent_test.go
    
    Co-authored-by: Ayush Thakur <100013900+ayusht2810@users.noreply.github.com>
    
    * [MI-2488] Modified some messages
    
    * [MI-2488] Fixed failing unit tests
    
    Co-authored-by: Ayush Thakur <100013900+ayusht2810@users.noreply.github.com>

---
+++ server/testutils/data.go
@@ -0,0 +1,28 @@
+package testutils
+
+import "github.com/mattermost/mattermost-server/v5/model"
+
+func GetID() string {
+	return "sfmq19kpztg5iy47ebe51hb31w"
+}
+
+func GetFile(deleted bool) *model.FileInfo {
+	file := &model.FileInfo{
+		CreatorId: GetID(),
+		ChannelId: GetID(),
+		MimeType:  "mockMimeType",
+		Name:      "mockName",
+	}
+
+	if deleted {
+		file.DeleteAt = 4324234
+	}
+
+	return file
+}
+
+func GetAppError(message string) *model.AppError {
+	return &model.AppError{
+		Message: message,
+	}
+}
