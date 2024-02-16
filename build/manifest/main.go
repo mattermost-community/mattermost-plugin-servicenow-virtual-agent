mattermost/mattermost-plugin-starter-template

Sync with playbooks: install-go-tools, gotestsum, and dynamic versions (#192)
* Revert "Update main.go (#154)"

This reverts commit be4a281d0cc791d10e6e5ae917b325b2f054e475.

* Revert "[MM-33506] Use embed package to include plugin manifest (#145)"

This reverts commit ca9ee3c17c6920a636a33f378e17395afd6f329f.

* Revert "Don't generate manifest.ts (#127)"

This reverts commit 18d30b50bc7ba800c9f05bfd82970781db0aea3e.

* install-go-tools target, adopt gotestsum

* bring back make apply + automatic versioning

* Update build/manifest/main.go

Co-authored-by: Michael Kochell <6913320+mickmister@users.noreply.github.com>

* suppress git describe error when no tags match

* make version/release notes opt-in

* fix whitespace in Makefile

* document version management options

---------

Co-authored-by: Michael Kochell <6913320+mickmister@users.noreply.github.com>

build/manifest/main.go
@@ -4,9 +4,48 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/mattermost/mattermost/server/public/model"
 	"github.com/pkg/errors"
+)
+
+const pluginIDGoFileTemplate = `// This file is automatically generated. Do not modify it manually.
+
+package main
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/mattermost/mattermost/server/public/model"
+)
+
+var manifest *model.Manifest
+
+const manifestStr = ` + "`" + `
+%s
+` + "`" + `
+
+func init() {
+	_ = json.NewDecoder(strings.NewReader(manifestStr)).Decode(&manifest)
+}
+`
+
+const pluginIDJSFileTemplate = `// This file is automatically generated. Do not modify it manually.
+
+const manifest = JSON.parse(` + "`" + `
+%s
+` + "`" + `);
+
+export default manifest;
+`
+
+// These build-time vars are read from shell commands and populated in ../setup.mk
+var (
+	BuildHashShort  string
+	BuildTagLatest  string
+	BuildTagCurrent string
 )
 
 func main() {
@@ -37,6 +76,16 @@ func main() {
 			fmt.Printf("true")
 		}
 
+	case "apply":
+		if err := applyManifest(manifest); err != nil {
+			panic("failed to apply manifest: " + err.Error())
+		}
+
+	case "dist":
+		if err := distManifest(manifest); err != nil {
+			panic("failed to write manifest to dist directory: " + err.Error())
+		}
+
 	default:
 		panic("unrecognized command: " + cmd)
 	}
@@ -62,6 +111,32 @@ func findManifest() (*model.Manifest, error) {
 		return nil, errors.Wrap(err, "failed to parse manifest")
 	}
 
+	// If no version is listed in the manifest, generate one based on the state of the current
+	// commit, and use the first version we find (to prevent causing errors)
+	if manifest.Version == "" {
+		var version string
+		tags := strings.Fields(BuildTagCurrent)
+		for _, t := range tags {
+			if strings.HasPrefix(t, "v") {
+				version = t
+				break
+			}
+		}
+		if version == "" {
+			if BuildTagLatest != "" {
+				version = BuildTagLatest + "+" + BuildHashShort
+			} else {
+				version = "v0.0.0+" + BuildHashShort
+			}
+		}
+		manifest.Version = strings.TrimPrefix(version, "v")
+	}
+
+	// If no release notes specified, generate one from the latest tag, if present.
+	if manifest.ReleaseNotesURL == "" && BuildTagLatest != "" {
+		manifest.ReleaseNotesURL = manifest.HomepageURL + "releases/tag/" + BuildTagLatest
+	}
+
 	return &manifest, nil
 }
 
@@ -73,4 +148,64 @@ func dumpPluginID(manifest *model.Manifest) {
 // dumpPluginVersion writes the plugin version from the given manifest to standard out
 func dumpPluginVersion(manifest *model.Manifest) {
 	fmt.Printf("%s", manifest.Version)
+}
+
+// applyManifest propagates the plugin_id into the server and webapp folders, as necessary
+func applyManifest(manifest *model.Manifest) error {
+	if manifest.HasServer() {
+		// generate JSON representation of Manifest.
+		manifestBytes, err := json.MarshalIndent(manifest, "", "  ")
+		if err != nil {
+			return err
+		}
+		manifestStr := string(manifestBytes)
+
+		// write generated code to file by using Go file template.
+		if err := os.WriteFile(
+			"server/manifest.go",
+			[]byte(fmt.Sprintf(pluginIDGoFileTemplate, manifestStr)),
+			0600,
+		); err != nil {
+			return errors.Wrap(err, "failed to write server/manifest.go")
+		}
+	}
+
+	if manifest.HasWebapp() {
+		// generate JSON representation of Manifest.
+		// JSON is very similar and compatible with JS's object literals. so, what we do here
+		// is actually JS code generation.
+		manifestBytes, err := json.MarshalIndent(manifest, "", "    ")
+		if err != nil {
+			return err
+		}
+		manifestStr := string(manifestBytes)
+
+		// Escape newlines
+		manifestStr = strings.ReplaceAll(manifestStr, `\n`, `\\n`)
+
+		// write generated code to file by using JS file template.
+		if err := os.WriteFile(
+			"webapp/src/manifest.ts",
+			[]byte(fmt.Sprintf(pluginIDJSFileTemplate, manifestStr)),
+			0600,
+		); err != nil {
+			return errors.Wrap(err, "failed to open webapp/src/manifest.ts")
+		}
+	}
+
+	return nil
+}
+
+// distManifest writes the manifest file to the dist directory
+func distManifest(manifest *model.Manifest) error {
+	manifestBytes, err := json.MarshalIndent(manifest, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(fmt.Sprintf("dist/%s/plugin.json", manifest.Id), manifestBytes, 0600); err != nil {
+		return errors.Wrap(err, "failed to write plugin.json")
+	}
+
+	return nil
 }

mattermost-community/mattermost-plugin-servicenow-virtual-agent

build/manifest/main.go
@@ -4,9 +4,48 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/mattermost/mattermost/server/public/model"
 	"github.com/pkg/errors"
+)
+
+const pluginIDGoFileTemplate = `// This file is automatically generated. Do not modify it manually.
+
+package plugin
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/mattermost/mattermost/server/public/model"
+)
+
+var manifest *model.Manifest
+
+const manifestStr = ` + "`" + `
+%s
+` + "`" + `
+
+func init() {
+	_ = json.NewDecoder(strings.NewReader(manifestStr)).Decode(&manifest)
+}
+`
+
+const pluginIDJSFileTemplate = `// This file is automatically generated. Do not modify it manually.
+
+const manifest = JSON.parse(` + "`" + `
+%s
+` + "`" + `);
+
+export default manifest;
+`
+
+// These build-time vars are read from shell commands and populated in ../setup.mk
+var (
+	BuildHashShort  string
+	BuildTagLatest  string
+	BuildTagCurrent string
 )
 
 func main() {
@@ -37,6 +76,16 @@ func main() {
 			fmt.Printf("true")
 		}
 
+	case "apply":
+		if err := applyManifest(manifest); err != nil {
+			panic("failed to apply manifest: " + err.Error())
+		}
+
+	case "dist":
+		if err := distManifest(manifest); err != nil {
+			panic("failed to write manifest to dist directory: " + err.Error())
+		}
+
 	default:
 		panic("unrecognized command: " + cmd)
 	}
@@ -62,6 +111,32 @@ func findManifest() (*model.Manifest, error) {
 		return nil, errors.Wrap(err, "failed to parse manifest")
 	}
 
+	// If no version is listed in the manifest, generate one based on the state of the current
+	// commit, and use the first version we find (to prevent causing errors)
+	if manifest.Version == "" {
+		var version string
+		tags := strings.Fields(BuildTagCurrent)
+		for _, t := range tags {
+			if strings.HasPrefix(t, "v") {
+				version = t
+				break
+			}
+		}
+		if version == "" {
+			if BuildTagLatest != "" {
+				version = BuildTagLatest + "+" + BuildHashShort
+			} else {
+				version = "v0.0.0+" + BuildHashShort
+			}
+		}
+		manifest.Version = strings.TrimPrefix(version, "v")
+	}
+
+	// If no release notes specified, generate one from the latest tag, if present.
+	if manifest.ReleaseNotesURL == "" && BuildTagLatest != "" {
+		manifest.ReleaseNotesURL = manifest.HomepageURL + "releases/tag/" + BuildTagLatest
+	}
+
 	return &manifest, nil
 }
 
@@ -73,4 +148,64 @@ func dumpPluginID(manifest *model.Manifest) {
 // dumpPluginVersion writes the plugin version from the given manifest to standard out
 func dumpPluginVersion(manifest *model.Manifest) {
 	fmt.Printf("%s", manifest.Version)
+}
+
+// applyManifest propagates the plugin_id into the server and webapp folders, as necessary
+func applyManifest(manifest *model.Manifest) error {
+	if manifest.HasServer() {
+		// generate JSON representation of Manifest.
+		manifestBytes, err := json.MarshalIndent(manifest, "", "  ")
+		if err != nil {
+			return err
+		}
+		manifestStr := string(manifestBytes)
+
+		// write generated code to file by using Go file template.
+		if err := os.WriteFile(
+			"server/plugin/manifest.go",
+			[]byte(fmt.Sprintf(pluginIDGoFileTemplate, manifestStr)),
+			0600,
+		); err != nil {
+			return errors.Wrap(err, "failed to write server/plugin/manifest.go")
+		}
+	}
+
+	if manifest.HasWebapp() {
+		// generate JSON representation of Manifest.
+		// JSON is very similar and compatible with JS's object literals. so, what we do here
+		// is actually JS code generation.
+		manifestBytes, err := json.MarshalIndent(manifest, "", "    ")
+		if err != nil {
+			return err
+		}
+		manifestStr := string(manifestBytes)
+
+		// Escape newlines
+		manifestStr = strings.ReplaceAll(manifestStr, `\n`, `\\n`)
+
+		// write generated code to file by using JS file template.
+		if err := os.WriteFile(
+			"webapp/src/manifest.ts",
+			[]byte(fmt.Sprintf(pluginIDJSFileTemplate, manifestStr)),
+			0600,
+		); err != nil {
+			return errors.Wrap(err, "failed to open webapp/src/manifest.ts")
+		}
+	}
+
+	return nil
+}
+
+// distManifest writes the manifest file to the dist directory
+func distManifest(manifest *model.Manifest) error {
+	manifestBytes, err := json.MarshalIndent(manifest, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(fmt.Sprintf("dist/%s/plugin.json", manifest.Id), manifestBytes, 0600); err != nil {
+		return errors.Wrap(err, "failed to write plugin.json")
+	}
+
+	return nil
 }