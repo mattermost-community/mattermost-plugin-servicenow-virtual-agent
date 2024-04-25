6a6e3811c9222ed82f82bd78f0f585f6dcfaa572
ayusht2810 <ayush.thakur@brightscout.com>
2024-04-25 16:15:05 +0530

    [MM-75] Revert mock command and move to custom.mk file

--- build/custom.mk
+++ build/custom.mk
@@ -1,1 +1,10 @@
 # Include custom targets and environment variables here
+
+## Generates mock golang interfaces for testing
+.PHONY: mock
+mock:
+ifneq ($(HAS_SERVER),)
+	go install github.com/golang/mock/mockgen@v1.6.0
+	mockgen -destination server/mocks/mock_store.go github.com/mattermost/mattermost-plugin-servicenow-virtual-agent/server/plugin Store
+	mockgen -destination server/mocks/mock_client.go github.com/mattermost/mattermost-plugin-servicenow-virtual-agent/server/plugin Client
+endif
